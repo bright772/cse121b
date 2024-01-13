2239d0f47d4b154385d131bca181d366cf2114f4
bright772 <90653984+bright772@users.noreply.github.com>
2024-01-13 13:30:45 +0900

    Update w01-task.js

--- w01-task.js
+++ w01-task.js
@@ -1,11 +1,11 @@
 // 🔍 Part 1 error
-const userName = "Moroni";
+let userName = "Moroni"; // Changed from 'const' to 'let'.
 console.log(`Username: ${userName}`);
 userName = "Moronihah";
 console.log(`Username: ${userName}`);
 
 // 🔍 Part 2 error
-const currentDateAndTime = DateTime;
+const currentDateAndTime = Date(); // Changed from 'DateTime' to 'Date()'.
 console.log(`It is now ${currentDateAndTime}`);
 
 // 🔍 Part 3 error. The following statement calls a function named total that accepts any number of arguments and returns the sum. The returned value is stored in a variable named theTotal. 1-10 are the arguments.
@@ -14,9 +14,9 @@ let theTotal = total(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 console.log(`The total is ${theTotal}`);
 
 // 'total' function declaration
-function total(...theNumbers) {
+function total(...theNumbers) { // The ellipsis is the rest/spread operator: packs parametric iterables into arrays, and spreads arrays into constituents.
 	let sum = 0;
-	for (let aNumber in theNumbers) {
+	for (let aNumber of theNumbers) { // Changed 'let X in Y' (taking indices) into 'let X of Y' (taking values).
 		sum += aNumber * 1; // Why do we use * 1? It implicitly converts a string to a number.
 	}
 	return sum
