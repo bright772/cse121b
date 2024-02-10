558967844e234e91fa17d1d97e0f35924c7a504d
bright772 <90653984+bright772@users.noreply.github.com>
2024-02-10 12:36:42 +0900

    Updated W05 task.

--- scripts/w05-task.js
+++ scripts/w05-task.js
@@ -6,15 +6,19 @@ const templeList = [];
 
 /* async displayTemples Function */
 const displayTemples = (array) => {
-    array.forEach(templeElement => {
+    array.forEach(templeObject => {
         const newArticleElement = document.createElement("article");
+
         const newH3Element = document.createElement("h3");
-        newH3Element.textContent(templeElement["templeName"]);
+        newH3Element.textContent = (templeObject["templeName"]);
+
         const newImgElement = document.createElement("img");
-        newImgElement.setAttribute("src", templeElement["imageUrl"]);
-        newImgElement.alt = templeElement.location;
+        newImgElement.setAttribute("src", templeObject["imageUrl"]);
+        newImgElement.alt = templeObject.location;
+
         newArticleElement.appendChild(newH3Element);
         newArticleElement.appendChild(newImgElement);
+
         templesElement.appendChild(newArticleElement);
     });
 }
@@ -22,20 +26,67 @@ const displayTemples = (array) => {
 /* async getTemples Function using fetch()*/
 const getTemples = async () => {
     const response = await fetch("https://byui-cse.github.io/cse121b-ww-course/resources/temples.json");
+    console.log(response);
     if (response.ok) {
         const data = await response.json();
         data.forEach((temple) => {
-            templeList.add(temple);
+            templeList.push(temple);
         })
     }
+    displayTemples(templeList);
 }
 
 /* reset Function */
-
+function reset() {
+    // Erase everything in the division.
+    templesElement.innerHTML = "";
+    console.log(templesElement.innerHTML);
+}
 
 /* filterTemples Function */
-
+function filterTemples(arrayOfTemples) {
+    reset();
+    const filter = document.getElementById("filtered");
+    switch (filter.value) {
+        case "utah":
+            displayTemples(arrayOfTemples.filter((eachObjectInArray) => {
+                return (
+                    eachObjectInArray.templeName.includes("Utah") || 
+                    eachObjectInArray.templeName.includes("Salt Lake") || 
+                    eachObjectInArray.templeName.includes("Provo")
+                    )
+                }));
+            break; //  
+        case "notutah":
+            displayTemples(arrayOfTemples.filter(temObj => {
+                return (
+                    !temObj.templeName.includes("Utah") && 
+                    !temObj.templeName.includes("Salt Lake") && 
+                    !temObj.templeName.includes("Provo")
+                    )
+                }));
+            break;
+        case "older":
+            displayTemples(arrayOfTemples.filter(function(object) {
+                dedicatedYear = parseInt(object["dedicated"].slice(0, 4));
+                return dedicatedYear < 1950;
+            }));
+            break;
+        case "all":
+            displayTemples(arrayOfTemples);
+        default:
+            break;
+    }
+}
 
 getTemples();
 
 /* Event Listener */
+const globalFilterElement = document.querySelector("#filtered");
+globalFilterElement.addEventListener("change", ( () => { filterTemples(templeList) } ) );
+
+/* Add more functionality */
+const newOptionElement = globalFilterElement.createElement("option");
+newOptionElement.setAttribute("value", "alphabetic");
+newOptionElement.innerText = "Sort alphabetically";
+globalFilterElement.appendChild(newOptionElement);
